113e38b45e6d4b911197fc5ebd0a6621bf2af741
weyewe <w.yunnal@gmail.com>
2012-02-19 14:12:02 +0700

    commenting is done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,30 @@
 class CommentsController < ApplicationController
   def create_first_child_comment
+    save_comment
+    
+    respond_to do |format|
+      format.html {  redirect_to project_submission_picture_path(@picture ,@picture) }
+      format.js
+    end
+    
+  end
+  
+  
+  
+  
+  def create_child_comment
+    
+    save_comment
+    respond_to do |format|
+      format.html {  redirect_to project_submission_picture_path(@picture ,@picture) }
+      format.js
+    end
+    
+    
+  end
+  
+  protected
+  def save_comment
     @root_comment = Comment.find_by_id(params[:root_comment_id])
     @picture = Picture.find_by_id( params[:picture_id])
 
@@ -7,14 +32,5 @@ class CommentsController < ApplicationController
     @comment = Comment.build_from( @picture, current_user.id, params[:comment][:body] )
     @comment.save
     @comment.move_to_child_of(@root_comment)
-    
-    
-    respond_to do |format|
-      format.html {  redirect_to project_submission_picture_path(@picture ,@picture) }
-      format.js
-    end
-    
-    
-    
   end
 end
